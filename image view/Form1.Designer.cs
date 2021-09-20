9555708c360c860c55f60095a06caf158d3dcc1e
hamza ziouane <zioianhamza@gmail.com>
2021-09-20 16:20:28 +0100

    Add project files.

---
+++ image view/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace image_view
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(463, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(473, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(69, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(299, 198);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(500, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "image number : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(654, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(603, 303);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 32);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(95, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Dénomination";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(296, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(486, 390);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ image view/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace image_view
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MemoryStream memory = new MemoryStream();
+        SqlConnection connection = new SqlConnection(@"data source = AANDROID-123122\SQLEXPRESS; initial catalog = img; integrated security = true");
+        SqlConnection connectionpub = new SqlConnection(@"server =192.168.100.92;database = simpleDatabase ; user id = log1; password=P@ssword1965** ;MultipleActiveResultSets =True;");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        //    SqlCommand command = new SqlCommand("select id from img", connection);
+        //    connection.Open();
+        //    SqlDataReader reader = command.ExecuteReader();
+        //    while (reader.Read())
+        //    {
+        //        comboBox1.Items.Add(reader[0].ToString());
+        //    }
+            
+
+            
+        //    reader.Close();
+        //    connection.Close();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("select * from img", connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            label2.Text = reader[0].ToString();
+            byte[] b = (byte[])reader[1];
+            //clear memry stream for another pic
+            memory.SetLength(0);
+            memory.Write(b, 0, b.Length);
+
+            pictureBox1.Image = Bitmap.FromStream(memory);
+            connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+            pictureBox2.Image = Bitmap.FromFile(dialog.FileName);
+            pictureBox2.Image.Save(memory, ImageFormat.Png);
+            byte[] b = memory.ToArray();
+            //Image b = pictureBox2.Image;
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into img (img)values ( @img )", connection);
+          //  command.Parameters.AddWithValue("@id", 3);
+            command.Parameters.Add("@img", SqlDbType.Image);
+            command.Parameters["@img"].SqlValue = b;
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            MessageBox.Show("image add");
+
+            comboBox1.Items.Clear();
+            SqlCommand command2 = new SqlCommand("select id from img", connection);
+            connection.Open();
+            SqlDataReader reader = command2.ExecuteReader();
+            while (reader.Read())
+            {
+                comboBox1.Items.Add(reader[0].ToString());
+            }
+            reader.Close();
+            connection.Close();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            SqlCommand command = new SqlCommand("select img from img where id = "+ comboBox1.SelectedItem.ToString()+"", connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            byte[] b = (byte[])reader[0];
+            //clear memry stream for another pic
+            memory.SetLength(0);
+            memory.Write(b, 0, b.Length);
+            label2.Text = comboBox1.SelectedItem.ToString();
+            
+            pictureBox1.InitialImage = null;
+            pictureBox1.Image = Bitmap.FromStream(memory);
+            reader.Close();
+            connection.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+            pictureBox2.Image = Bitmap.FromFile(dialog.FileName);
+            pictureBox2.Image.Save(memory, ImageFormat.Png);
+            byte[] b = memory.ToArray();
+            //Image b = pictureBox2.Image;
+            connectionpub.Open();
+            SqlCommand command = new SqlCommand("insert into Sigle (Sigle,Dénomination,Parti)values(@Sigle,@Dénomination, @Parti)", connectionpub);
+
+            command.Parameters.AddWithValue("@Sigle",textBox1.Text);
+            command.Parameters.AddWithValue("@Dénomination",b);
+            //command.Parameters.Add("@Dénomination", SqlDbType.Image);
+            //command.Parameters["@Dénomination"].SqlValue = b;
+            command.Parameters.AddWithValue("@Parti", textBox2.Text);
+
+
+            command.ExecuteNonQuery();
+
+            connectionpub.Close();
+            MessageBox.Show("image add");
+
+            comboBox1.Items.Clear();
+            //SqlCommand command2 = new SqlCommand("select id from img", connection);
+            //connection.Open();
+            //SqlDataReader reader = command2.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    comboBox1.Items.Add(reader[0].ToString());
+            //}
+            //reader.Close();
+            //connection.Close();
+        }
+    }
+}
